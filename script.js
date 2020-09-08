e74f1b8d91f56d63fb721638ce8d75a2fb5a59c8
Yaron <yaronleo11@gmail.com>
2020-09-08 16:58:55 +0300

    add repeat and if

--- script.js
+++ script.js
@@ -10,13 +10,33 @@ const personalMovieDB = {
     private: false
 };
 
-const lastMovie = prompt("Один из последних просмотренных фильмов?", ""),
-      rateMovie = prompt("На сколько оцените его?", ""),
-      lastMovie2 = prompt("Один из последних просмотренных фильмов?", ""),
-      rateMovie2 = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотреных вами фильмов?', '');
+    const b = prompt('на сколько оцените его?' );
 
-personalMovieDB.movies[lastMovie] = rateMovie;
-personalMovieDB.movies[lastMovie2] = rateMovie2;
+    if (a === null || b === null) {
+        i--;
+        continue;
+    }
+
+    if (a.length > 50 || b.langth > 50) {
+        i--;
+        continue;
+    } 
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
