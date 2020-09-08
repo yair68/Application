1e6dea8bbce46cb1997faf95f746c475a77e36a9
Yaron <yaronleo11@gmail.com>
2020-09-08 14:43:12 +0300

    created DB of movies + asked some questions

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 "use strict";
-alert(1);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastMovie = prompt("Один из последних просмотренных фильмов?", ""),
+      rateMovie = prompt("На сколько оцените его?", ""),
+      lastMovie2 = prompt("Один из последних просмотренных фильмов?", ""),
+      rateMovie2 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastMovie] = rateMovie;
+personalMovieDB.movies[lastMovie2] = rateMovie2;
+
+console.log(personalMovieDB);
